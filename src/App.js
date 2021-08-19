c5f7c2edae69269cffbe3bc59c13f2d9904bb5f5
rgoshen <rick.goshen@gmail.com>
2021-08-18 21:12:27 -0700

    created shoppage and colleciton preview components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 class App extends Component {
   render() {
@@ -17,7 +12,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
