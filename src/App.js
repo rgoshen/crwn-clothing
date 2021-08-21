9f8a34d99cae2ce6011e0b3bcabda0d2e6745199
rgoshen <rick.goshen@gmail.com>
2021-08-21 15:36:02 -0700

    added app ability to know about user auth

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInandSignUpPage from "./pages/signin-signup-page/signin-signup-page.component";
+import { auth } from "./firebase/firebase.utils";
 
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    //opens the subscription and returns a function that can close the subscription
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth(); //closes the subscription
+  }
+
   render() {
     return (
       <div className="App">
