5e8c81e91ac9318a8444db34760924857d02be30
rgoshen <rick.goshen@gmail.com>
2021-08-26 14:25:01 -0700

    fixed bad import of connect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import { Connect } from "react-redux";
+import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-icon.styles.scss";
-import { connect } from "react-redux";
 
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
