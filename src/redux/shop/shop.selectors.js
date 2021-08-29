7815f1e94f766865fea4978e772829f2363f4796
rgoshen <rick.goshen@gmail.com>
2021-08-29 15:07:43 -0700

    normalized individual colletion data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,6 @@
 import { createSelector } from "reselect";
 import { memoize } from "lodash";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,8 +9,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
