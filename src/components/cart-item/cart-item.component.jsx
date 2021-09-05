5fc02d8d8be8848c81f1aeacbf43749c093d5ffe
rgoshen <rick.goshen@gmail.com>
2021-09-04 18:18:07 -0700

    refactored cart-icon and cart-item to styled comp

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import {
+  CartItemContainer,
+  CartItemImage,
+  CartItemDetailContainer,
+} from "./cart-item.styles";
 
-import "./cart-item.styles.scss";
+// import "./cart-item.styles.scss";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt="item" />
+    <CartItemDetailContainer>
+      <span>{name}</span>
+      <span>
         {quantity} x ${price}
       </span>
-    </div>
-  </div>
+    </CartItemDetailContainer>
+  </CartItemContainer>
 );
 
 export default CartItem;
