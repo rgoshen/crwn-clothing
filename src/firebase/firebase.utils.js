07e148c78d1c700533546950167710d9cf8e70be
rgoshen <rick.goshen@gmail.com>
2021-09-17 17:28:20 -0700

    refactored google sign in to users.sagas

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -82,8 +82,8 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 // Setup for Google authentication
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" }); //always trigger google popup
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" }); //always trigger google popup
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
